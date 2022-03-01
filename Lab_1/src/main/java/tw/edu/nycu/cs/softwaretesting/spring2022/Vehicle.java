376f73a7b35a23ce730e44038ae3410111fdd86d
t106362512 <t106362512@gmail.com>
2022-03-01 14:20:58 +0800

    feat(Lab_1): Convert to module and maven project

--- Lab_1/src/main/java/tw/edu/nycu/cs/softwaretesting/spring2022/Vehicle.java
+++ Lab_1/src/main/java/tw/edu/nycu/cs/softwaretesting/spring2022/Vehicle.java
@@ -1,10 +1,14 @@
+package tw.edu.nycu.cs.softwaretesting.spring2022;
+
 class Vehicle {
+    private static int numVehicle = 0; // Class Variable
     private int speed; // Object Variable
     private String direction; // Object Variable, direction is a reference to String Object
-    private static int numVehicle = 0; // Class Variable
+
     public Vehicle() { // Constructor, called when new an Object
-        this(0,"north"); // call another constructor to do initialization
+        this(0, "north"); // call another constructor to do initialization
     }
+
     public Vehicle(int s, String dir) { // Another Constructor. Use overloading to define two constructors
         float speed; // define a local variable
         speed = s; // the speed here refers to the above local variable
@@ -12,23 +16,29 @@ class Vehicle {
         direction = dir; // dir is a reference to object, not the object itself
         numVehicle++;   // increase the Vehicle number
     }
+
+    public static int totalVehicle() { // Class Method
+        return numVehicle;
+    }
+
     protected void finalize() { // Destructor, called when the object is garbage collected by JVM
         System.out.println("finalize has been called");
         numVehicle--;
     }
-    void setSpeed(int newSpeed) { // Object Method
-        this.speed = newSpeed;
-    }
-    void setDir(String dir) { // Object Method
-        this.direction = dir;
-    }
+
     int getSpeed() { // Object Method
         return speed;
     }
+
+    void setSpeed(int newSpeed) { // Object Method
+        this.speed = newSpeed;
+    }
+
     String getDir() { // Object Method
         return direction;
     }
-    public static int totalVehicle() { // Class Method
-        return numVehicle;
+
+    void setDir(String dir) { // Object Method
+        this.direction = dir;
     }
 }
