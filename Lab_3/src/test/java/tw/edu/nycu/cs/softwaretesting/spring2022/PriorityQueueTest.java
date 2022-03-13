8d088abbeb06b299bf55d019cca764fdc7312943
t106362512 <t106362512@gmail.com>
2022-03-14 01:52:42 +0800

    test(Lab_3): Remove own PriorityQueue

--- Lab_3/src/test/java/tw/edu/nycu/cs/softwaretesting/spring2022/PriorityQueueTest.java
+++ Lab_3/src/test/java/tw/edu/nycu/cs/softwaretesting/spring2022/PriorityQueueTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
